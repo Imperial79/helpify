5286a2fded8f296b0c776919e4db0511f3a188d7
Soumik Sil <soumiksilco@gmail.com>
2024-03-24 00:02:13 +0530

    updated Screens/Profile.jsx

--- frontend/src/Screens/Profile.jsx
+++ frontend/src/Screens/Profile.jsx
@@ -1,6 +1,36 @@
 import React from "react";
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 function Profile() {
+  const [user, setUser] = useState({});
+  const [posts, setPosts] = useState([]);
+  const userID = window.localStorage.getItem("userID");
+  //fetching User Data and User's Posts
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/users/profile/${userID}`
+        );
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+    fetchUser();
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/posts/profile-post/${userID}`
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
       <div className="flex flex-col">
@@ -13,9 +43,9 @@ function Profile() {
             />
           </div>
           <div className="flex flex-col">
-            <h1 className="text-xl font-medium">h/user00898</h1>
-            <h1 className="text-sm text-gray-500">@username</h1>
-            <h1 className="text-sm text-gray-500 font-medium mt-2">Posts 10</h1>
+            <h1 className="text-xl font-medium">{user.name}</h1>
+            <h1 className="text-sm text-gray-500">{user.email}</h1>
+            <h1 className="text-sm text-gray-500 font-medium mt-2">Posts {posts.length}</h1>
           </div>
         </div>
 
@@ -25,11 +55,12 @@ function Profile() {
         <p className="text-gray-600 font-medium mb-5">Your Posts</p>
 
         <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
+          {posts.map((post)=>{
+            return (<>
+                <PostCard key={post._id} title={post.title} content={post.content}/>
+            </>)
+          })}
+
         </div>
       </div>
     </div>
@@ -38,7 +69,7 @@ function Profile() {
 
 export default Profile;
 
-function PostCard() {
+function PostCard({title,content}) {
   return (
     <div className="bg-gray-100 rounded-xl p-5">
       <div className="w-full h-[200px] rounded-xl bg-white mb-2">
@@ -50,14 +81,11 @@ function PostCard() {
       </div>
 
       <h2 className="font-medium text-gray-700 mb-2">
-        [title] Blood Donation Camp starts 22nd April
+        {title}
       </h2>
 
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem harum
-        deleniti laudantium ducimus eos earum quibusdam, provident laborum
-        distinctio nobis exercitationem omnis ipsum, quasi fuga nostrum sed
-        molestiae labore voluptatibus!
+        {content}
       </p>
     </div>
   );
