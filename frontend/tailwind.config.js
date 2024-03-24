23f9dcf6ef0692dae0e47589058f604c054697f5
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-03-24 16:27:13 +0000

    Added dark mode functionality and set to media

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "*"],
+  darkMode: "media", // class, 'media' or boolean
   theme: {
     extend: {},
   },
