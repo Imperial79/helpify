b581b5c2fb1367ccb31dce218e07fbba7c707e9b
Imperial79 <avishekverma79@gmail.com>
2024-03-26 13:47:13 +0530

    fix

--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -1,15 +1,21 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Scaffold from "../components/Scaffold";
-import { MenuIcon } from "../components/Icons";
+import {
+  CloseIcon,
+  ImageIcon,
+  LocationIcon,
+  MenuIcon,
+} from "../components/Icons";
 import { Context } from "../context/ContextProvider";
-import { useNavigate } from "react-router-dom";
+import Modal from "../components/Modal";
 
 function Home() {
-  const { userID } = useContext(Context);
   const [usersList, setUsers] = useState([]);
   const [posts, setPosts] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [showCreatePostModal, setShowCreatePostModal] = useState(false);
+
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -51,52 +57,167 @@ function Home() {
     <Scaffold isLoading={isLoading}>
       {/* POST FORM */}
 
-      <div className="p-4 mt-4 rounded-lg bg-white dark:bg-dark-second">
-        <div className="p-2 flex items-center gap-2">
-          <img
-            src="https://source.unsplash.com/random"
-            alt="Profile Picture"
-            className="rounded-full w-10 h-10"
-          />
-          <div className="flex-1 bg-gray-100 rounded-full flex items-center justify-start pl-4 cursor-pointer dark:bg-dark-third text-gray-500 text-lg dark:text-dark-txt">
-            <span>What's on your mind, {"SomeUserName"}?</span>
+      <div className="max-w-2xl mx-auto">
+        <div>
+          <div className="p-4 rounded-lg bg-white">
+            <div className="p-2 flex items-center gap-2">
+              <div className="h-10 w-10 overflow-hidden rounded-full flex-shrink-0">
+                <img
+                  src="https://source.unsplash.com/random"
+                  alt="profile-img"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <button
+                type="button"
+                onClick={() => {
+                  setShowCreatePostModal(true);
+                }}
+                className="text-start text-sm font-medium text-gray-400 rounded-xl bg-gray-100 p-3 w-full"
+              >
+                <span>🤔 What's on your mind, {"SomeUserName"}?</span>
+              </button>
+            </div>
+          </div>
+          <div>
+            {/* POST */}
+            {posts
+              .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+              .map((post) => {
+                const currentUser = usersList.find(
+                  (user) => user._id === post.user_id
+                );
+                return currentUser ? (
+                  <div key={post._id}>
+                    <PostComponent
+                      postID={post._id}
+                      currentUser={currentUser}
+                      title={post.title}
+                      content={post.content}
+                      likes={post.likes}
+                      createdAt={post.createdAt}
+                      onDelete={deletePost}
+                    />
+                  </div>
+                ) : (
+                  <></>
+                );
+              })}
           </div>
         </div>
       </div>
 
-      {/* END POST FORM */}
-
-      {/* LIST POST */}
-      <div>
-        {/* POST */}
-        {posts
-          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-          .map((post) => {
-            const currentUser = usersList.find(
-              (user) => user._id === post.user_id
-            );
-            return currentUser ? (
-              <div key={post._id}>
-                <PostComponent
-                  postID={post._id}
-                  currentUser={currentUser}
-                  title={post.title}
-                  content={post.content}
-                  likes={post.likes}
-                  createdAt={post.createdAt}
-                  onDelete={deletePost}
-                />
-              </div>
-            ) : (
-              <></>
-            );
-          })}
-      </div>
+      {/* Create POST Modal */}
+      <CreatePostModal
+        showPostModal={showCreatePostModal}
+        setShowPostModal={setShowCreatePostModal}
+      />
     </Scaffold>
   );
 }
 
 export default Home;
+
+function CreatePostModal({ showPostModal, setShowPostModal }) {
+  const [imagePreview, setImagePreview] = useState(null);
+  const [postContent, setPostContent] = useState("");
+
+  function handlePostSubmit() {}
+  return (
+    <Modal
+      isOpen={showPostModal}
+      toggleModal={() => {
+        setShowPostModal(!showPostModal);
+      }}
+    >
+      <div>
+        <div className="flex items-center justify-between mb-5">
+          <h1 className="font-medium text-xl">Create Post</h1>
+        </div>
+        <div className="flex gap-2 items-center mb-5">
+          <div className="circleAvatar bg-gray-100">
+            <img src="https://source.unsplash.com/random" alt="" />
+          </div>
+          <div>
+            <h1 className="font-medium">username</h1>
+            <div className="flex items-center gap-2 text-sm text-gray-500 font-medium">
+              <LocationIcon size={"h-5 w-5"} color={"text-blue-700"} />
+              Durgapur, West Bengal
+            </div>
+          </div>
+        </div>
+        {/* Add Image */}
+        {imagePreview == null ? (
+          <button
+            onClick={() => {
+              document.getElementById("postImagePicker").click();
+            }}
+            className="w-full rounded-xl mb-5 flex items-center gap-2 bg-gray-100 p-2 justify-center hover:bg-gray-200"
+          >
+            <ImageIcon size={"h-5 w-5"} color={"text-gray-500"} />
+            <h1 className="font-medium text-gray-500">Add Image</h1>
+          </button>
+        ) : (
+          <div className="w-full rounded-xl h-[200px] overflow-hidden relative mb-5">
+            <img
+              src={imagePreview}
+              alt="picked-image"
+              className="h-full w-full object-contain"
+            />
+            <button
+              type="button"
+              onClick={() => {
+                setImagePreview(null);
+              }}
+              className="absolute rounded-full h-7 w-7 bg-black right-[10px] top-[10px] flex justify-center items-center"
+            >
+              <CloseIcon color={"text-white object-contain mx-auto"} />
+            </button>
+          </div>
+        )}
+        <input
+          type="file"
+          id="postImagePicker"
+          className="hidden"
+          accept=".jpeg, .jpg, .png, .webp"
+          onChange={(e) => {
+            setImagePreview(URL.createObjectURL(e.target.files[0]));
+          }}
+        />
+
+        {/* Post Body */}
+        <textarea
+          name="postContent"
+          id="postContent"
+          rows={6}
+          className="w-full p-2 bg-gray-100 rounded-xl mb-5"
+          placeholder="What's on your mind?"
+          onChange={(e) => {
+            setPostContent(e.target.value);
+          }}
+        ></textarea>
+        {/* Create Post Button and Cancel Button */}
+        <div className="flex gap-2">
+          <button
+            type="button"
+            onClick={handlePostSubmit}
+            className="kButton w-full"
+          >
+            Create Post
+          </button>
+          <button
+            className="min-w-[200px] bg-black rounded-full text-white hover:bg-gray-700"
+            onClick={() => {
+              setShowPostModal(false);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
 
 const PostComponent = ({
   postID,
@@ -231,12 +352,12 @@ const PostComponent = ({
       <div className="text-justify px-4 py-2">{content}</div>
       {/* END POST CONTENT */}
       {/* POST IMAGE */}
-      <div className="py-2">
-        {/* <img
-          className="h-64 w-64 mx-auto"
+      <div className="bg-gray-100">
+        <img
+          className="max-h-[300px] mx-auto"
           src="https://source.unsplash.com/random"
-          alt="Post image"
-        /> */}
+          alt="post-image"
+        />
       </div>
       {/* END POST IMAGE */}
       {/* POST REACT */}
