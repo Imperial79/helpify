e716835e4f90d25adf5c3af1fd292532293c5073
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-03-29 10:10:57 +0000

    Added Comment Section Template

--- frontend/src/components/PostComponent.jsx
+++ frontend/src/components/PostComponent.jsx
@@ -178,6 +178,147 @@ export const PostComponent = ({
           </div>
         </div>
       </div>
+      
+      {/* Comment Section */}
+      <div>
+        {/* LIST COMMENT */}
+        <div className="py-2 px-4">
+          {/* COMMENT */}
+          <div className="flex space-x-2">
+            <img
+              src="./images/avt-5.jpg"
+              alt="Profile picture"
+              className="w-9 h-9 rounded-full"
+            />
+            <div>
+              <div className="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                <span className="font-semibold block">John Doe</span>
+                <span>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </span>
+              </div>
+              <div className="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                <span className="font-semibold cursor-pointer">Like</span>
+                <span>.</span>
+                <span className="font-semibold cursor-pointer">Reply</span>
+                <span>.</span>
+                10m
+              </div>
+              {/* COMMENT */}
+              <div className="flex space-x-2">
+                <img
+                  src="./images/avt-7.jpg"
+                  alt="Profile picture"
+                  className="w-9 h-9 rounded-full"
+                />
+                <div>
+                  <div className="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                    <span className="font-semibold block">John Doe</span>
+                    <span>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </span>
+                  </div>
+                  <div className="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                    <span className="font-semibold cursor-pointer">Like</span>
+                    <span>.</span>
+                    <span className="font-semibold cursor-pointer">Reply</span>
+                    <span>.</span>
+                    10m
+                  </div>
+                </div>
+              </div>
+              {/* END COMMENT */}
+            </div>
+          </div>
+          {/* END COMMENT */}
+          {/* COMMENT */}
+          <div className="flex space-x-2">
+            <img
+              src="./images/avt-5.jpg"
+              alt="Profile picture"
+              className="w-9 h-9 rounded-full"
+            />
+            <div>
+              <div className="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                <span className="font-semibold block">John Doe</span>
+                <span>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+                  voluptate ipsa animi corrupti unde, voluptatibus expedita
+                  suscipit, itaque, laudantium accusantium aspernatur officia
+                  repellendus nihil mollitia soluta distinctio praesentium nulla
+                  eos?
+                </span>
+              </div>
+              <div className="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                <span className="font-semibold cursor-pointer">Like</span>
+                <span>.</span>
+                <span className="font-semibold cursor-pointer">Reply</span>
+                <span>.</span>
+                10m
+              </div>
+              {/* COMMENT */}
+              <div className="flex space-x-2">
+                <img
+                  src="./images/avt-7.jpg"
+                  alt="Profile picture"
+                  className="w-9 h-9 rounded-full"
+                />
+                <div>
+                  <div className="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                    <span className="font-semibold block">John Doe</span>
+                    <span>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </span>
+                  </div>
+                  <div className="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                    <span className="font-semibold cursor-pointer">Like</span>
+                    <span>.</span>
+                    <span className="font-semibold cursor-pointer">Reply</span>
+                    <span>.</span>
+                    10m
+                  </div>
+                </div>
+              </div>
+              {/* END COMMENT */}
+            </div>
+          </div>
+          {/* END COMMENT */}
+        </div>
+        {/* END LIST COMMENT */}
+        {/* COMMENT FORM */}
+        <div className="py-2 px-4">
+          <div className="flex space-x-2">
+            <img
+              src="./images/tuat.jpg"
+              alt="Profile picture"
+              className="w-9 h-9 rounded-full"
+            />
+            <div className="flex-1 flex bg-gray-100 dark:bg-dark-third rounded-full items-center justify-between px-3">
+              <input
+                type="text"
+                placeholder="Write a comment..."
+                className="outline-none bg-transparent flex-1"
+              />
+              <div className="flex space-x-0 items-center justify-center">
+                <span className="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl">
+                  <i className="bx bx-smile" />
+                </span>
+                <span className="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl">
+                  <i className="bx bx-camera" />
+                </span>
+                <span className="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl">
+                  <i className="bx bxs-file-gif" />
+                </span>
+                <span className="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl">
+                  <i className="bx bx-happy-heart-eyes" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* END COMMENT FORM */}
+      </div>
+
       {/* END POST ACTION */}
     </div>
   );
