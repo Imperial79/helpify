687abe451d303be97540c95fa87057aced8c59cd
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-04-08 15:41:31 +0000

    Fix

--- backend/models/User_model.js
+++ backend/models/User_model.js
@@ -1,17 +1,5 @@
 import mongoose from "mongoose";
 
-// const UserSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   latitude: { type: String, required: true },
-//   longitude: { type: String, required: true },
-//   location_details: {
-//     place_id: { type: String, required: true },
-//     city: { type: String, required: true },
-//   },
-// });
-
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
